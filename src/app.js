463ece5679e54e01fad476379587cb8cf40db620
Javier Salafica <jsalafica@gmail.com>
2023-07-08 19:09:35 -0300

    actualizaciones

--- src/app.js
+++ src/app.js
@@ -7,21 +7,12 @@ const PORT = 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use("/api", router);
-
-// app.use((req, res) => {
-//   res.json({
-//     error: -2,
-//     desc: `Route ${req.path} method ${req.method} doesn't exist`,
-//   });
-// });
-
 app.use("/", router);
 
 app.listen(PORT, (error) => {
   if (error) {
     console.log(error);
   } else {
-    console.log("Server listening on port " + PORT);
+    console.log(`Server listening on port ${PORT}`);
   }
 });
--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -2,42 +2,123 @@ import { Router } from "express";
 import fs from "fs";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
+import { ProductManager } from "../ProductManager.js";
 
 const router = Router();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const products = [];
+const productManager = new ProductManager("products.json");
 
 router
   .route("/")
-  .get((req, res) => {
-    res.json({ msg: "Muestro todos los productos", products: products });
-  })
-  .post((req, res) => {
-    const { product } = req.body;
-    let newProductId;
-    if (products.length == 0) {
-      newProductId = 1;
-    } else {
-      newProductId = products[products.length - 1].id + 1;
+  .get(async (req, res) => {
+    const { limit } = req.query;
+    try {
+      let response = await productManager.getProducts();
+      if (limit) {
+        let tempArray = response.filter((dat, index) => index < limit);
+        res.json({ data: tempArray, limit: limit, quantity: tempArray.length });
+      } else {
+        res.json({ data: response, limit: false, quantity: response.length });
+      }
+    } catch (err) {
+      console.log(err);
     }
-
-    // products.push(product);
-    res.json({ msg: "Producto agregado correctamente", producto: product });
+  })
+  .post(async (req, res) => {
+    const {
+      title,
+      description,
+      code,
+      price,
+      status,
+      stock,
+      category,
+      thumbnails,
+    } = req.body;
+    try {
+      let response = await productManager.addProduct(
+        title,
+        description,
+        price,
+        thumbnails,
+        code,
+        stock,
+        status,
+        category
+      );
+      res.json({
+        message: "producto agregado",
+        data: response,
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({
+        message: "error interno del servidor",
+      });
+    }
   });
 
 router
   .route("/:pid")
-  .get((req, res) => {
+  .get(async (req, res) => {
     const { pid } = req.params;
-    res.json({ status: 200, data: "Muestro producto con id: " + pid });
+    let product = await productManager.getProductById(Number(pid));
+    if (product) {
+      res.json({ message: "success", data: product });
+    } else {
+      res.json({
+        message: "error",
+        data: "El producto solicitado no existe",
+      });
+    }
   })
-  .put((req, res) => {
+  .put(async (req, res) => {
     const { pid } = req.params;
-    res.json({ status: 200, data: "Edito producto con id: " + pid });
+    const {
+      title,
+      description,
+      price,
+      thumbnails,
+      code,
+      stock,
+      status,
+      category,
+    } = req.body;
+    let product = await productManager.updateProductById(
+      Number(pid),
+      title,
+      description,
+      price,
+      thumbnails,
+      code,
+      stock,
+      status,
+      category
+    );
+    if (product) {
+      res.json({
+        message: "Producto actualizado correctamente",
+        data: product,
+      });
+    } else {
+      res.json({
+        message: "error",
+        data: "El producto solicitado no existe",
+      });
+    }
   })
-  .delete((req, res) => {
+  .delete(async (req, res) => {
     const { pid } = req.params;
-    res.json({ status: 200, data: "Borro producto con id: " + pid });
+    let product = await productManager.deleteProductById(Number(pid));
+    if (product) {
+      res.json({ message: "Producto borrado exitosamente", data: product });
+    } else {
+      res.json({
+        message: "error",
+        data: "El producto solicitado no existe",
+      });
+    }
   });
 
 export default router;
